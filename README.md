8d22f7065a0e4b059ee3dd724db93a00d6617c81
Bobby Drake <bobby.drake@hal24k.com>
2021-11-19 02:16:59 +0100

    updated training

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# mongodb-training
+# MongoDb Training
+
 Training material for MongoDb Cross-Team Training Day
+
+## Prerequisites
+
+- [Python 3.8 or higher](https://www.python.org/downloads/release/python-380/)
+- [Pipenv](https://pipenv.pypa.io/en/latest/)
+
+## Installation
+
+Use `pipenv` to install the necessary dependencies.
+
+```shell
+$ pipenv install --dev
+```
+
+Start the `pipenv` shell with the following command
+
+```shell
+$ pipenv shell
+```
+
+## Data Modelling
+
+The `data_modelling` directory contains the code for the data-modelling lesson. To run the application
+use the following command
+
+```shell
+$ uvicorn data_modelling.main:app --reload
+```
+
+This repo contains a [Postman](https://www.postman.com/downloads/) collection for testing the API called `Book API.postman_collection.json`.
+This file can be imported into the Postman application.
+
+### Exercise 1
+
+The update endpoint allows `tags` to be updated. Right now it will overwrite the existing tags.
+Update the `update_book` function to add new tags without over-writing the old.
+
+### Exercise 2
+
+We want to know more information about the author. Convert the author property to a nested document including
+the following fields using a Pydantic model
+
+- bio - a text field where some information about the author can be stored
+- rating - a numerical field where users can rate the author with 1 - 10 stars in increments of 0.5
+- first_name - the author's first name
+- last_name - the author's last name
+
+Note: When updating a book, the author information should not be over-written.
+
+### Exercise 3
+
+We want to add chapters to the book with a chapter title. Use a Pydantic model with the following fields and add the
+chapter to the Book entity.
+
+- chapter_title - a text field of the chapter title. (i.e. Foreword, 1, Index)
+- synopsis - a text field with a summary of the chapter in a few sentences.
+- pages - the number of pages in the chapter
+
+## Transactions
+
+MongoDb 4.x supports ACID compliant transactions. To use transactions you will need to use the session object
+in a with-statement. [Documentation](https://pymongo.readthedocs.io/en/stable/api/pymongo/client_session.html#transactions)
+
+The `transactions` directory contains the code for the transactions lesson. To run the application
+use the following command
+
+```shell
+$ uvicorn transactions.main:app --reload
+```
+
+This repo contains a [Postman](https://www.postman.com/downloads/) collection for testing the API called `Webshop API.postman_collection.json`.
+This file can be imported into the Postman application.
+
+### Exercise 1
+
+The customer has two properties which need to be updated and saved back to the `customers` collection. Add an appropriate
+update statement.
+
+Use the data below to insert a new customer and a new order.
+
+#### Example Data
+
+Sample Customer Document
+
+```json
+{
+  "first_name": "John",
+  "last_name": "Doe",
+  "mobile_mobile": "0611111111",
+  "email_address": "john@email.com",
+  "total_purchased": 0,
+  "total_orders": 0
+}
+```
+
+Sample Order Document
+
+```json
+{
+  "customer_id": "<insert customer id>",
+  "items": [
+    {
+      "sku": "1111",
+      "qty": 1,
+      "price": 5.25
+    },
+    {
+      "sku": "2222",
+      "qty": 2,
+      "price": 11.5
+    }
+  ],
+  "total": 0
+}
+```
+
+### Bonus Exercise
+
+Implement a stock keeping collection which will keep the quantity of a given sku.
+When an item is sold the stock quantity should be decreased when a new order is created.
+
+Sample Stock Document
+
+```json
+{
+  "sku": "1111",
+  "qty": 100
+}
+```
---
+++ transactions/main.py
@@ -0,0 +1,195 @@
+from functools import reduce
+import os
+from typing import List, Any
+
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse, Response
+from pydantic import BaseModel, Field
+from pymongo import MongoClient
+from pymongo.database import Database
+
+from data_modelling.library import ObjectIdField, DomainException, to_objectid_str
+
+load_dotenv()
+
+app = FastAPI(
+    title="Webshop API",
+    version="1.0.0",
+    description="A webshop API written on top of MongoDb",
+)
+
+
+@app.exception_handler(DomainException)
+def handle_invalid_usage(response, error):
+    return JSONResponse(error.to_dict(), status_code=error.status_code)
+
+
+def get_db():
+    client = MongoClient(host=os.getenv("MONGODB_URI"))
+    return client.test_database
+
+
+def get_client():
+    return MongoClient(host=os.getenv("MONGODB_URI"))
+
+
+class NotFound(DomainException):
+    code = "NOT_FOUND"
+    status_code = 404
+
+
+# Orders
+
+
+class OrderItem(BaseModel):
+    sku: str
+    qty: int
+    price: float
+
+
+class OrderBase(BaseModel):
+    customer_id: ObjectIdField
+    total: float = Field(default=0)
+    items: List[OrderItem]
+
+
+class OrderAdd(OrderBase):
+    pass
+
+
+class OrderGet(OrderBase):
+    class Config:
+        json_encoders = {
+            ObjectId: to_objectid_str,
+        }
+
+    id: ObjectIdField = Field(alias='_id')
+
+
+class OrderAddResult(BaseModel):
+    class Config:
+        json_encoders = {
+            ObjectId: to_objectid_str,
+        }
+
+    id: ObjectIdField
+
+
+@app.get("/")
+async def index():
+    return {"application": "Webshop API"}
+
+
+def order_total(items: List[OrderItem]):
+    total = 0
+    for item in items:
+        total = total + (item.qty * item.price)
+    return total
+
+
+@app.post("/orders/", status_code=201, response_model=OrderAddResult, tags=["Orders"])
+async def add_order(order: OrderAdd, client: MongoClient = Depends(get_client)):
+    customers = client.test_database.customers
+    orders = client.test_database.orders
+
+    customer = customers.find_one(order.customer_id)
+
+    if not customer:
+        raise NotFound(f"Customer {order.customer_id} not found!")
+    customer = CustomerBase(**customer)
+
+    with client.start_session() as session:
+        with session.start_transaction():
+            total = order_total(order.items)
+            customer.total_orders = customer.total_orders + 1
+            customer.total_purchased = customer.total_purchased + total
+
+            # Exercise 1 - make sure the customer document is updated
+            # TODO Update Customer
+
+            # Insert order
+            order.total = total
+            result = orders.insert_one(order.dict())
+    return OrderAddResult(id=result.inserted_id)
+
+
+@app.delete("/orders/{id}", status_code=204, tags=["Orders"])
+async def delete_order(id: ObjectIdField, db: Database = Depends(get_db)):
+    db.orders.delete_one({"_id": id})
+    return Response(status_code=204)
+
+
+@app.get("/orders/", response_model=List[OrderGet], tags=["Orders"])
+async def get_orders(db: Database = Depends(get_db)):
+    orders = db.orders.find({})
+    return [OrderGet(**order) for order in orders]
+
+
+@app.get("/orders/{id}", response_model=OrderGet, tags=["Orders"])
+async def get_order(id: ObjectIdField, db: Database = Depends(get_db)):
+    order = db.orders.find_one(id)
+    if order:
+        return OrderGet(**order)
+    raise NotFound(f"Order by id {id!r} not found!")
+
+
+# Customers
+
+
+class CustomerBase(BaseModel):
+    first_name: str
+    last_name: str
+    mobile_mobile: str
+    email_address: str
+    total_purchased: float = Field(default=0)
+    total_orders: int = Field(default=0)
+
+
+class CustomerAdd(CustomerBase):
+    pass
+
+
+class CustomerUpdate(CustomerBase):
+    pass
+
+
+class CustomerGet(CustomerBase):
+    class Config:
+        json_encoders = {
+            ObjectId: to_objectid_str,
+        }
+
+    id: ObjectIdField = Field(alias='_id')
+
+
+class CustomerAddResult(BaseModel):
+    class Config:
+        json_encoders = {
+            ObjectId: to_objectid_str,
+        }
+
+    id: ObjectIdField
+
+
+@app.post(
+    "/customers/", status_code=201, response_model=CustomerAddResult, tags=["Customers"]
+)
+async def add_customer(customer: CustomerAdd, db: Database = Depends(get_db)):
+    result = db.customers.insert_one(customer.dict())
+    return CustomerAddResult(id=result.inserted_id)
+
+
+@app.put("/customers/{id}", status_code=204, tags=["Customers"])
+async def update_customer(
+    id: ObjectIdField, customer: CustomerUpdate, db: Database = Depends(get_db)
+):
+    db.customers.update_one({"_id": id}, {"$set": customer.dict()})
+    return Response(status_code=204)
+
+
+@app.get("/customers/", response_model=List[CustomerGet], tags=["Customers"])
+async def get_customers(db: Database = Depends(get_db)):
+    customers = db.customers.find({})
+    return [CustomerGet(**customer) for customer in customers]
